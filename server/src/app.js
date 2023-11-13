74f1b3231f07488fe32438bcdaa91694b1c6fd8c
Daniel Pericich <dpericich@gmail.com>
2023-11-12 18:10:19 -0600

    Updatd server app.js to include tmdb router

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
+import tmdbRouter from './routes/tmdb';
 
 const app = express();
 app.use(logger('dev'));
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
+app.use('/v1/tmdb', tmdbRouter);
 
 export default app;
