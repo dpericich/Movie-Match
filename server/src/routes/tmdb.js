d7708b83e1232ab4344f6f880a80a998f8343e7d
Daniel Pericich <dpericich@gmail.com>
2023-11-19 20:09:49 -0600

    Updated tmdb routes with new endpoints

--- server/src/routes/tmdb.js
+++ server/src/routes/tmdb.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { tmdbTopMovies } from '../controllers/tmdb';
+import { tmdbTopMovies, tmdbGenreList, tmdbMovieObject, tmdbGenreMovies } from '../controllers/tmdb';
 const tmdbRouter = express.Router();
 
 tmdbRouter.get('/top-movies', tmdbTopMovies);
+tmdbRouter.get('/genres', tmdbGenreList);
+tmdbRouter.get('/movie/:movieId', tmdbMovieObject);
+tmdbRouter.get('/genres/:genreName/:page?', tmdbGenreMovies);
 
 export default tmdbRouter;
