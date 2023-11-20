cd317874dd0c41da24546f2bf35dbfd8cff9a1a1
Daniel Pericich <dpericich@gmail.com>
2023-11-19 20:22:50 -0600

    Added optional page param to the top movies endpoint

--- server/src/routes/tmdb.js
+++ server/src/routes/tmdb.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { tmdbTopMovies, tmdbGenreList, tmdbMovieObject, tmdbGenreMovies } from '../controllers/tmdb';
 const tmdbRouter = express.Router();
 
-tmdbRouter.get('/top-movies', tmdbTopMovies);
+tmdbRouter.get('/top-movies/:page?', tmdbTopMovies);
 tmdbRouter.get('/genres', tmdbGenreList);
 tmdbRouter.get('/movie/:movieId', tmdbMovieObject);
 tmdbRouter.get('/genres/:genreName/:page?', tmdbGenreMovies);
