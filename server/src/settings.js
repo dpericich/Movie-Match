abcf4b11a4f25241120f2a83eb12945c4f3656b4
Daniel Pericich <dpericich@gmail.com>
2024-01-12 00:04:59 -0600

    Added postgres db env variable references

--- server/src/settings.js
+++ server/src/settings.js
@@ -3,3 +3,6 @@ dotenv.config();
 // Use non-name exports for all ENV variables
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
 export const tmdbApiKeyEnvironmentVariable = process.env.TMDB_API_KEY;
+export const postgresDatabaseName = process.env.POSTGRES_DB;
+export const postgresUser = process.env.POSTGRES_USER;
+export const postgresPassword = process.env.POSTGRES_PASSWORD;
